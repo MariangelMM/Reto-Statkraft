cdf4e95e90bc56498144e7a3540a1b2e8a4a5dc8
MariangelMM <mariangelmoramarquez@gmail.com>
2020-06-12 14:50:28 -0500

    Updates

---
+++ precache-manifest.5ee2a6293b3c6f6beaf29cc032e77f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67829de9f7a39f2e84097104c8cc3058",
+    "url": "/Reto-Statkraft/index.html"
+  },
+  {
+    "revision": "7f30ab074567e9541758",
+    "url": "/Reto-Statkraft/static/css/main.bbe6ac2b.chunk.css"
+  },
+  {
+    "revision": "0594a46e0f612dc7379a",
+    "url": "/Reto-Statkraft/static/js/2.212ac606.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/Reto-Statkraft/static/js/2.212ac606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f30ab074567e9541758",
+    "url": "/Reto-Statkraft/static/js/main.487a2a3f.chunk.js"
+  },
+  {
+    "revision": "7c9f069992c2c3780da6",
+    "url": "/Reto-Statkraft/static/js/runtime-main.702fbc90.js"
+  }
+]);
